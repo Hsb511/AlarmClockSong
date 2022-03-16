e3e7e5a225f15944758fc615499bf0c129eb3231
Hsb511 <hugo-selle@hotmail.fr>
2022-03-16 23:28:22 +0100

    [CLN] MainActivity

--- app/src/main/java/com/example/alarmclocksong/ui/MainActivity.kt
+++ app/src/main/java/com/example/alarmclocksong/ui/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.alarmclocksong.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import com.example.alarmclocksong.ui.theme.ACSTheme
 import com.example.alarmclocksong.ui.views.AlarmClockList
 
-class MainActivity:  ComponentActivity() {
+class MainActivity: ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { ACSTheme { AlarmClockList() } }
