c48755a042bcee41052c3d36b62a90746bb99666
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-04-15 21:19:49 +0200

    [UPD] Improving the build gradle + adding hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,23 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
+    val technicalVersion = 1
+    val prodVersion = 0
+    val stagingVersion = 0
+
     compileSdk = 32
 
     defaultConfig {
         applicationId = "com.example.alarmclocksong"
         minSdk = 26
         targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = technicalVersion * 10000 + prodVersion * 100 + stagingVersion
+        versionName = "$technicalVersion.$prodVersion.$stagingVersion"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
@@ -25,11 +31,26 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+        }
     }
+
+    android.applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .map{ it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach {
+                it.outputFileName = "fizzbuzzapp_${variant.buildType.name}_${defaultConfig.versionName}.apk"
+            }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -44,10 +65,16 @@ android {
 }
 
 dependencies {
+    // Core
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.core:core-ktx:1.7.0")
+
+    // With compose
     implementation("androidx.activity:activity-compose:1.6.0-alpha01")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-alpha05")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-alpha06")
+    implementation("androidx.navigation:navigation-compose:2.4.2")
+
+    // Compose
     implementation("androidx.compose.compiler:compiler:${rootProject.extra["composeVersion"]}")
     implementation("androidx.compose.ui:ui:${rootProject.extra["composeVersion"]}")
     implementation("androidx.compose.ui:ui-viewbinding:${rootProject.extra["composeVersion"]}")
@@ -55,9 +82,16 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["composeVersion"]}")
     implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["composeVersion"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["composeVersion"]}")
-    implementation("androidx.navigation:navigation-compose:2.4.1")
+
+    // Material
     implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["composeVersion"]}")
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.40.5")
+    kapt("com.google.dagger:hilt-android-compiler:2.40.5")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
